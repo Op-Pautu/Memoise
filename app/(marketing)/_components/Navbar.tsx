10860f1182f1dea1647aaf927d7b7bf695753246
Op-Pautu <pautuop@gmail.com>
2024-01-30 01:57:07 +0530

    Finish landing page

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -3,19 +3,20 @@
 import { useScrollTop } from "@/hooks/useScrollTop";
 import { cn } from "@/lib/utils";
 import Logo from "./Logo";
+import { ModeToggle } from "@/components/modeToggle";
 
 const Navbar = () => {
   const scrolled = useScrollTop();
   return (
     <div
       className={cn(
-        "z-50 bg-background fixed top-0 flex items-center w-full p-6",
+        "z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6",
         scrolled && "border-b shadow-sm"
       )}
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
-        Login
+        <ModeToggle />
       </div>
     </div>
   );
