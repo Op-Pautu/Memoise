302c7bea65b2203910011c84efac66ad74404787
Op-Pautu <pautuop@gmail.com>
2024-02-01 22:05:40 +0530

    Add functionality to add notes and nested notes

--- app/(main)/_components/Navigation.tsx
+++ app/(main)/_components/Navigation.tsx
@@ -12,15 +12,15 @@ import { usePathname } from "next/navigation";
 import React, { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import UserItem from "./UserItem";
-import { useMutation, useQuery } from "convex/react";
+import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import Item from "./Item";
 import { toast } from "sonner";
+import DocumentList from "./DocumentList";
 
 const Navigation = () => {
   const pathname = usePathname();
 
-  const documents = useQuery(api.documents.get);
   const create = useMutation(api.documents.create);
 
   const isMobile = useMediaQuery("(max-width: 768px)");
@@ -143,9 +143,7 @@ const Navigation = () => {
         </div>
 
         <div className="mt-4">
-          {documents?.map((document) => (
-            <p key={document._id}>{document.title}</p>
-          ))}
+          <DocumentList />
         </div>
 
         <div
