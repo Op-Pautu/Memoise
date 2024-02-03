1383a8a5291987120bf43dec2cc532d94678de55
Op-Pautu <pautuop@gmail.com>
2024-02-03 20:07:35 +0530

    Add search functionality

--- app/(main)/_components/Item.tsx
+++ app/(main)/_components/Item.tsx
@@ -126,7 +126,7 @@ const Item = ({
 
       {isSearch && (
         <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-          <span className="text-xs">⌘</span>K
+          <span className="text-[10px]">Ctrl</span>K
         </kbd>
       )}
 
--- app/(main)/_components/Navigation.tsx
+++ app/(main)/_components/Navigation.tsx
@@ -25,10 +25,11 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 import TrashBox from "./TrashBox";
+import { useSearch } from "@/hooks/useSearch";
 
 const Navigation = () => {
   const pathname = usePathname();
-
+  const search = useSearch();
   const create = useMutation(api.documents.create);
 
   const isMobile = useMediaQuery("(max-width: 768px)");
@@ -145,7 +146,7 @@ const Navigation = () => {
 
         <div>
           <UserItem />
-          <Item label="Search" icon={Search} isSearch onClick={() => {}} />
+          <Item label="Search" icon={Search} isSearch onClick={search.onOpen} />
           <Item label="Settings" icon={Settings} onClick={() => {}} />
           <Item onClick={handleCreate} label="New page" icon={PlusCircle} />
         </div>
