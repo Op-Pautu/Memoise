03e1452a72d094841a7017a99a16370348a9c70d
Op-Pautu <pautuop@gmail.com>
2024-02-04 22:36:49 +0530

    Refactor code for better UX

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import Cover from "@/components/Cover";
-import Editor from "@/components/Editor";
 import Toolbar from "@/components/Toolbar";
+
 import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { useMutation, useQuery } from "convex/react";
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
 
 interface DocumentIdPageProps {
   params: {
@@ -14,6 +16,11 @@ interface DocumentIdPageProps {
   };
 }
 const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+  const Editor = useMemo(
+    () => dynamic(() => import("@/components/Editor"), { ssr: false }),
+    []
+  );
+
   const document = useQuery(api.documents.getById, {
     documentId: params.documentId,
   });
--- app/(main)/_components/Navigation.tsx
+++ app/(main)/_components/Navigation.tsx
@@ -10,7 +10,7 @@ import {
   Settings,
   Trash,
 } from "lucide-react";
-import { useParams, usePathname } from "next/navigation";
+import { useParams, usePathname, useRouter } from "next/navigation";
 import React, { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import UserItem from "./UserItem";
@@ -32,6 +32,7 @@ import Navbar from "./Navbar";
 const Navigation = () => {
   const pathname = usePathname();
   const params = useParams();
+  const router = useRouter();
 
   const search = useSearch();
   const settings = useSettings();
@@ -119,8 +120,9 @@ const Navigation = () => {
   };
 
   const handleCreate = () => {
-    const promise = create({ title: "Untitled" });
-
+    const promise = create({ title: "Untitled" }).then((documentId) =>
+      router.push(`/documents/${documentId}`)
+    );
     toast.promise(promise, {
       loading: "Creating a new note...",
       success: "New note created!",
